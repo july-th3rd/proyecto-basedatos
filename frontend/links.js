fd13315703d4620fcd6e0073e195a87289e5fad7
Predatorzz1 <168335407+Predatorzz1@users.noreply.github.com>
2024-11-13 16:20:44 -0300

    Add files via upload

--- frontend/links.js
+++ frontend/links.js
@@ -4,3 +4,57 @@ function redireccionInvitado(){
 function redireccionLogin(){
     location.href = "admin.html"
 }
+function redireccionClientes(){
+    location.href = "clientes.html"
+}
+function redireccionCuentaBancaria(){
+    location.href = "cuentabancaria.html"
+}
+function redireccionTrabajadores(){
+    location.href = "trabajadores.html"
+}
+function redireccionRoles(){
+    location.href = "roles.html"
+}
+function redireccionPromoción(){
+    location.href = "promocion.html"
+}
+function redireccionRegiones(){
+    location.href = "regiones.html"
+}
+function redireccionCiudades(){
+    location.href = "ciudades.html"
+}
+function redireccionCalleDireccion(){
+    location.href = "calledireccion.html"
+}
+function redireccionInsumo(){
+    location.href = "insumo.html"
+}
+function redireccionProveedor(){
+    location.href = "proveedor.html"
+}
+function redireccionProductos(){
+    location.href = "productos.html"
+}
+function redireccionInventario(){
+    location.href = "inventario.html"
+}
+function redireccionPagoCliente(){
+    location.href = "pagocliente.html"
+}
+function redireccionPagoProveedor(){
+    location.href = "pagoproveedor.html"
+}
+function func1(){
+    location.href = "func1.html"
+}
+function func2(){
+    location.href = "func2.html"
+}
+function func3(){
+    location.href = "func3.html"
+}
+
+
+
